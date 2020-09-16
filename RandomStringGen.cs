a144a31a2c707e17c8deabbc601fb49f64d317dc
Matheus Pessoa <matheuspessoax@gmail.com>
2020-09-16 08:34:30 -0300

    Add files via upload

---
+++ RandomStringGen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RandomString
+{
+    class RandomStringGen
+    {
+        static void Main() {
+            for (int i = 0; i <= 100; i++) 
+            {
+                Console.WriteLine(RandomStringGen()); 
+            }
+        }
+        public static string RandomStringGen()
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[60];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            var finalString = new String(stringChars);
+            return finalString;
+            
+        }
+    }
+}
